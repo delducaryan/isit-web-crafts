ebf082d9910d6cef3f1421836b76b32010514238
Ryan <ryanofthedukes@gmail.com>
2017-10-22 22:36:22 -0700

    webcrafts reactstarter

--- source/ReactHome.js
+++ source/ReactHome.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import HomeButtons from './HomeButtons';
 
 export default class ReactHome extends React.Component {
     render() {
-        return <h1>An H1 element in a React Component</h1>;
+        return (
+			<div>
+				<h1>An H1 element in a React Component</h1>
+				<HomeButtons />
+			</div>
+		)
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './public/javascripts/main.js',
+    entry: './source/react-main.js',
     output: {path: __dirname, filename: 'bundle.js'},
     module: {
         loaders: [
