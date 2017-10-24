ec447e4894d0bd8167d6ff9aa765f380447ae379
Ryan-BC <delduca.ryan@gmail.com>
2017-10-23 17:10:53 -0700

    end of inclass

---
+++ source/__tests__/sanity.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ElfDebugEnzyme from '../ElfDebugEnzyme';
+const elfDebugEnzyme = new ElfDebugEnzyme(true, 'sanity');
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({adapter: new Adapter()});
+
+import ReactHome from '../ReactHome';
+import HomeButtons from '../HomeButtons';
+
+describe('basic sanity test', function() {
+
+    it('is true true?', function() {
+        expect(true).toBe(true);
+    });
+
+    it('tests if we can load ReactHome', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<ReactHome />, div);
+    });
+
+    it('tests if we can load HomeButtons', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<HomeButtons />, div);
+    });
+
+    it('renders default value of H1 tag', () => {
+        const wrapper = shallow(<ReactHome />);
+        const nineSign = <h1>An H1 element in a React Component</h1>;
+        elfDebugEnzyme.getLast(wrapper, 'h1', true);
+        expect(wrapper.contains(nineSign)).toEqual(true);
+    })
+});
