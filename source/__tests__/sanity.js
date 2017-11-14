a46fb58b3e29564bd64aaf94c84436404d11f2dd
Ryan-BC <delduca.ryan@gmail.com>
2017-11-13 16:51:34 -0800

    inclass mocks

--- source/__tests__/sanity.js
+++ source/__tests__/sanity.js
@@ -9,6 +9,11 @@ const elfDebugEnzyme = new ElfDebugEnzyme(true, 'sanity');
 
 import ReactHome from '../ReactHome';
 import HomeButtons from '../HomeButtons';
+import MakeHtml from '../MakeHtml';
+import MakeHtmlDropDowns from '../MakeHtmlDropDowns';
+import MakeHtmlHomeButton from '../MakeHtmlHomeButton';
+import MakeImages from '../MakeImages';
+import MakeImagesButtons from '../MakeImagesButtons';
 import raf from '../temp-poly-fills';
 
 describe('basic sanity test', function() {
@@ -27,4 +32,28 @@ describe('basic sanity test', function() {
         ReactDOM.render(<HomeButtons />, div);
     });
 
+    it('renders MakeHtml without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<MakeHtml />, div);
+    });
+
+    it('renders MakeHtmlDropDowns without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<MakeHtmlDropDowns />, div);
+    });
+
+    it('renders MakeHtmlHomeButton without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<MakeHtmlHomeButton />, div);
+    });
+
+    it('renders MakeImages without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<MakeImages />, div);
+    });
+
+    it('renders MakeImagesButtons without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<MakeImagesButtons />, div);
+    });
 });
